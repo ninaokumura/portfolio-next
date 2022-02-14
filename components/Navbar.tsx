a84e16a87d5b4541fb4cd6018f6b5af3d5cd3689
Nina Okumura <elaineokumura@gmail.com>
2022-02-14 15:43:03 +1300

    implement active route state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import clsx from 'clsx'
 
 const links = [
   {
@@ -17,6 +19,8 @@ const links = [
 ]
 
 export default function Navbar() {
+  const router = useRouter()
+  const currentPath = router.asPath
   return (
     <div className="flex flex-1 items-center justify-end">
       <ul className="flex items-center gap-4">
@@ -26,9 +30,14 @@ export default function Navbar() {
             className="flex min-w-[125px] flex-1 justify-center whitespace-nowrap py-2"
           >
             <Link href={link.href}>
-              <span className="cursor-pointer text-xl text-terracota opacity-90 transition-transform hover:scale-[102%] hover:font-bold">
+              <a
+                className={clsx(
+                  'cursor-pointer text-xl text-terracota opacity-90 transition-transform hover:scale-[102%] hover:font-bold',
+                  { 'font-bold': link.href === currentPath }
+                )}
+              >
                 {link.label}
-              </span>
+              </a>
             </Link>
           </li>
         ))}
