529219d86639074fb43508397c530a7665fd9354
Nina Okumura <elaineokumura@gmail.com>
2022-02-19 20:25:26 +1300

    re-add form

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,16 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <form
+          name="contact"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <textarea name="message"></textarea>
+        </form>
       </body>
     </Html>
   )
