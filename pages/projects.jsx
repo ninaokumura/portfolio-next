a2ef8b936269fe402fa9454d4cafdb5638ad41b0
Nina Okumura <elaineokumura@gmail.com>
2022-02-20 18:01:18 +1300

    extract project data to a another file

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -2,72 +2,7 @@ import React from 'react'
 import MainLayout from '../layouts/MainLayout'
 import Card from '../components/Card'
 import Button from '../components/Button'
-
-const projects = [
-  {
-    img: require('../public/images/portfolio.jpg'),
-    title: 'Personal Portfolio',
-    // body: 'Portfolio - FreeCodeCamp',
-    codeUrl: 'https://codesandbox.io/s/personal-portfolio-764y8',
-    demoUrl: 'https://764y8.csb.app/',
-  },
-  {
-    img: require('../public/images/omnifood.jpg'),
-    title: 'Food App',
-    // body: 'Omnifood is a food app delivery service.',
-    codeUrl: 'https://github.com/ninaokumura/omnifood',
-    demoUrl: 'https://omnifood-nina.netlify.app/',
-  },
-  {
-    img: require('../public/images/guess-number.jpg'),
-    title: 'Guess the Number game',
-    // body: 'Guess the number between 1 and 20',
-    codeUrl: 'https://github.com/ninaokumura/guess-the-number',
-    demoUrl: 'https://ninaokumura.github.io/guess-the-number/',
-  },
-  {
-    img: require('../public/images/forkify.jpg'),
-    title: 'Forkify',
-    // body: 'Recipe app',
-    codeUrl: 'https://github.com/ninaokumura/forkify-app',
-    demoUrl: 'https://forkify-nina.netlify.app/',
-  },
-  {
-    img: require('../public/images/personal-blog.jpg'),
-    title: 'Personal blog',
-    // body: 'First project built as part of Bootcamp',
-    codeUrl: 'https://github.com/ninaokumura/ninaokumura.github.io',
-    demoUrl: 'https://ninaokumura.github.io/',
-  },
-  {
-    img: require('../public/images/blog.jpg'),
-    title: 'Brogui',
-    // body: 'Just a blog.',
-    codeUrl: 'https://github.com/ninaokumura/brogui',
-    demoUrl: 'https://brogui.herokuapp.com/',
-  },
-  {
-    img: require('../public/images/natours.jpg'),
-    title: 'Natours',
-    // body: 'Travel ecommerce app',
-    codeUrl: 'https://github.com/ninaokumura/brogui',
-    demoUrl: 'https://brogui.herokuapp.com/',
-  },
-  {
-    img: require('../public/images/calculator.jpg'),
-    title: 'Calculator',
-    // body: 'Calculator buil with React',
-    codeUrl: 'https://github.com/ninaokumura/react-calculator',
-    demoUrl: 'https://ninaokumura.github.io/react-calculator/',
-  },
-  {
-    img: require('../public/images/crown.jpg'),
-    title: 'Crown clothing',
-    // body: 'E-commerce app built using React',
-    codeUrl: 'https://github.com/ninaokumura/react-crown-clothing',
-    demoUrl: 'https://crown-clothing-42.herokuapp.com/',
-  },
-]
+import projects from './project-data.js'
 
 export default function Projects() {
   return (
