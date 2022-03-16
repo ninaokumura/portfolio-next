895b1cda456fc3ef7fd9133c9c3fe0ca8e44b162
Nina Okumura <elaineokumura@gmail.com>
2022-03-17 00:13:22 +1300

    styling improvements

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -4,6 +4,32 @@ import Card from '../components/Card'
 import Button from '../components/Button'
 import projects from '../data/projects.data'
 
+import { FaReact, FaNodeJs } from 'react-icons/fa'
+import { SiMongodb, SiTailwindcss } from 'react-icons/si'
+import { DiJavascript, DiCss3, DiHtml5 } from 'react-icons/di'
+
+import { darken } from 'polished'
+
+const icons = {
+  react: FaReact,
+  css: DiCss3,
+  mongodb: SiMongodb,
+  node: FaNodeJs,
+  tailwind: SiTailwindcss,
+  html: DiHtml5,
+  javascript: DiJavascript,
+}
+
+const iconsColors = {
+  react: darken(0.1, '#61DBFB'),
+  html: '#e34f26',
+  css: '#0098FF',
+  tailwind: darken(0.2, '#8be9fd'),
+  javascript: darken(0.15, '#F0DB4F'),
+  mongodb: '#3FA037',
+  node: '#215732',
+}
+
 export default function Projects() {
   return (
     <MainLayout title="projects">
@@ -12,13 +38,30 @@ export default function Projects() {
           <h1 className="text-dark inline-block text-xl font-semibold sm:text-3xl"></h1>
         </header>
 
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {projects.map((el) => (
-            <Card key={el.title}>
-              <Card.Img src={el.img} />
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {projects.map((item) => (
+            <Card key={item.title}>
+              <Card.Img src={item.img} />
 
               <Card.Body>
-                <Card.Title>{el.title}</Card.Title>
+                <Card.Title>{item.title}</Card.Title>
+
+                <div className="flex items-center justify-center gap-2">
+                  {item.techIconsId.map((icon) => {
+                    const Icon = icons[icon.id]
+                    const color = iconsColors[icon.id]
+
+                    return (
+                      <span
+                        key={icon.id}
+                        style={{ color: color }}
+                        className="text-[26px]"
+                      >
+                        <Icon />
+                      </span>
+                    )
+                  })}
+                </div>
               </Card.Body>
               <div className="flex w-full gap-2">
                 <Button className="flex-1">
@@ -26,7 +69,7 @@ export default function Projects() {
                     target="_blank"
                     rel="noreferrer noopener"
                     className="projects-btn"
-                    href={el.codeUrl}
+                    href={item.codeUrl}
                   >
                     Code
                   </a>
@@ -36,7 +79,7 @@ export default function Projects() {
                     target="_blank"
                     rel="noreferrer noopener"
                     className="projects-btn"
-                    href={el.demoUrl}
+                    href={item.demoUrl}
                   >
                     Demo
                   </a>
